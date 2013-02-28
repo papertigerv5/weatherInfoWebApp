aa340578f3efd0a5bb07cae8800af5a1c1cd4838
papertigerv5 <papertigerv5@163.com>
2013-02-28 21:19:48 +0800

    网页页面过多的显示效果调整

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,35 +9,60 @@ import java.util.List;
 
 public class Application extends Controller {
 
-    public static void index(String cityName,int pageNumber) {
+    public static void index(String cityName,int checkedPageIndex,int startPageIndex) {
         System.out.println(cityName);
         String placeName;
         if(cityName == null){
             placeName = PLSINPUTCITYNAME;
             render(placeName);
         } else {
-            if(pageNumber == 0){
-                pageNumber = 1;
+            if(checkedPageIndex == 0){
+                checkedPageIndex = 1;
             }
-            int beginCount = (pageNumber - 1)* PAGE_COUNTS;
+
+            if(startPageIndex == 0){
+                startPageIndex = 1;
+            }
+
+            if(checkedPageIndex == startPageIndex + PAGE_SHOW_COUNT - 1){
+                startPageIndex = startPageIndex + PAGE_SHOW_COUNT - 1;
+            }else if(checkedPageIndex <= startPageIndex){
+                if(startPageIndex - PAGE_SHOW_COUNT > 0){
+                    startPageIndex = startPageIndex - PAGE_SHOW_COUNT;
+                } else {
+                    startPageIndex = 1;
+                }
+            }
+
+            int beginCount = (checkedPageIndex - 1)* PAGE_PER_COUNT;
             long satisfiedCount = WeatherInfoBean.count("districtName=?", cityName);
-            int totalPages = (int)(satisfiedCount/ PAGE_COUNTS);
-            totalPages = (int)satisfiedCount% PAGE_COUNTS ==0?totalPages:totalPages+1;
+            int totalPages = (int)(satisfiedCount/ PAGE_PER_COUNT);
+            totalPages = (int)satisfiedCount%PAGE_PER_COUNT ==0?totalPages:totalPages+1;
 
-            List<WeatherInfoBean> results = WeatherInfoBean.find("districtName=?",cityName).from(beginCount).fetch(PAGE_COUNTS);
+            List<WeatherInfoBean> results = WeatherInfoBean.find("districtName=? order by id desc",cityName).from(beginCount).fetch(PAGE_PER_COUNT);
 
-            List<Integer> pageNumberList = new ArrayList<Integer>();
             //Stupid solution.
-            for(int i = 1; i <= totalPages; i++){
-                pageNumberList.add(i);
+            List<Integer> pageNumberList = new ArrayList<Integer>();
+
+            int pageIndex = startPageIndex;
+            for(pageIndex = startPageIndex; pageIndex < startPageIndex + PAGE_SHOW_COUNT;){
+                if(pageIndex <= totalPages){
+                    pageNumberList.add(pageIndex);
+                    pageIndex++;
+                } else {
+                    break;
+                }
             }
-            System.out.println(results.size());
+
             placeName = cityName;
-            render(results,pageNumberList,pageNumber,placeName);
+            int pageShowCounts = PAGE_SHOW_COUNT;
+            render(results,pageNumberList,checkedPageIndex,placeName,totalPages,pageShowCounts,startPageIndex);
         }
     }
 
-    private static final int PAGE_COUNTS = 10;
+    private static final int PAGE_SHOW_COUNT = 15;
+    private static final int PAGE_PER_COUNT = 10;
     private static final String PLSINPUTCITYNAME = "请输入城市名称";
+    private static final String PLACEINDEX = "...";
 
 }
